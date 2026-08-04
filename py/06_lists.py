e49abe168e2345b946161b04bb6c1d88054e4a3b
defozecheng <defo.shin88@gmail.com>
2026-08-04 13:02:18 +0800

    your comment

---
+++ py/06_lists.py
@@ -0,0 +1,36 @@
+#List: A set of data within square brackets [...]
+
+fruits = ["apple","banana","orange"]
+numbers = [1,2,3,4,5]
+mixed = ["hello",42,3.14,True]
+empty_list = []
+
+# # Accessing Elements
+# print(fruits[0])
+# print(fruits[-1])
+# print(numbers[1:4])
+# print(numbers[:3])
+# print(numbers[2:])
+
+# Lists Operation: CRUD a list
+
+fruits.append("grape")
+fruits.insert(1,"kiwi")
+fruits.remove("banana")
+popped = fruits.pop()
+fruits.sort()
+fruits.reverse()
+
+# List operations
+len(fruits)
+"apple" in fruits
+fruits + ["mango"]
+fruits * 2
+
+edited_fruits = fruits.copy()
+edited_fruits.append("grape")
+
+print(edited_fruits)
+print(len(fruits))
+print(fruits)
+print(popped)
