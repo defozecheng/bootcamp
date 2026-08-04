cbf34726b3107482b8415fa693c8be356a078f6b
defozecheng <defo.shin88@gmail.com>
2026-08-04 21:10:22 +0800

    your comment

---
+++ py/01_variables_data_types.py
@@ -0,0 +1,42 @@
+# Write python code
+
+# name = "Bob"        # String
+# age = 10            # Integer
+# weight = 20.5       # Float
+# is_female = False   # Boolean
+
+# print(name)
+# print(type(name))
+
+# Math Operations
+
+# x = 10
+# y = 3
+
+# print(x + y)          # Addition
+# print(x - y)          # Substraction
+# print(x * y)          # Multiplication
+# print(x / y)          # Division
+# print(x // y)         # Floor Division
+# print(x % y)          # Modulus
+# print(x ** y)         # Exponentation
+
+
+
+# Exercise 1: Create variables that consist String, Integer, Float, and Booleandata types. Print value for each key.
+
+name = "Cheng"
+year = 1988
+height = 173.5
+is_male = True
+
+print(name)
+print(type(name))
+print(year)
+print(type(year))
+print(height)
+print(type(height))
+print(is_male)
+print(type(is_male))
+
+# Exercise 2: Convert Celsius to Fahrenheit. (F = C * 9/5 + 32)celcius = 25
