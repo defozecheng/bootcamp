64b9239dac38307a91cc2c23f9ae9efdae6fb202
defozecheng <defo.shin88@gmail.com>
2026-08-05 21:58:12 +0800

    your comment

--- py/05_loops.py
+++ py/05_loops.py
@@ -7,12 +7,27 @@
 
 # Exercise 2: Write a program that finds all prime numbers up to a given number. (limit=20)
 
+# Method 1
+
 for i in range(2,21):
     for j in range(2,i):
         if i % j == 0:
             break
     else:
         print(i)
+
+# Method 2
+limit = 20
+
+for num in range(2, limit +1):
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num)
+
 
 
 
