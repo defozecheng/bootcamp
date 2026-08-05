def5776b47e2898c896929a8826771b61ec2fa9c
defozecheng <defo.shin88@gmail.com>
2026-08-05 22:32:50 +0800

    your comment

--- py/03_io_validation.py
+++ py/03_io_validation.py
@@ -22,33 +22,55 @@
 #Exercise 1
 # Create a simple calculator that takes two number and an operation from user
 
-while True:
-    try:
-        Number_1 = int(input("Enter your first number:  "))
-        break
-    except ValueError:
-        print("Invalid number, please try again.")
+# while True:
+#     try:
+#         Number_1 = int(input("Enter your first number:  "))
+#         break
+#     except ValueError:
+#         print("Invalid number, please try again.")
 
-while True:
-    try:
-        Number_2 = int(input("Enter your second number:  "))
-        break
-    except ValueError:
-        print("Invalid number, please try again.")
-Operator = input("Enter an operator (+,-,*,/): ")
+# while True:
+#     try:
+#         Number_2 = int(input("Enter your second number:  "))
+#         break
+#     except ValueError:
+#         print("Invalid number, please try again.")
+# Operator = input("Enter an operator (+,-,*,/): ")
 
-while Operator not in ["+","-","*","/"]:
-    print ("Invalid Opetator, please try again.")
-    Operator = input("Enter an operator (+,-,*,/): ")
+# while Operator not in ["+","-","*","/"]:
+#     print ("Invalid Opetator, please try again.")
+#     Operator = input("Enter an operator (+,-,*,/): ")
 
-if Operator == "+":
-    print (f"{Number_1}{Operator}{Number_2}={Number_1 + Number_2}")
-elif Operator == "-":
-    print (f"{Number_1}{Operator}{Number_2}={Number_1 - Number_2}")
-elif Operator == "*":
-    print (f"{Number_1}{Operator}{Number_2}={Number_1 * Number_2}")
-elif Operator == "/":
-    print (f"{Number_1}{Operator}{Number_2}={Number_1 / Number_2}")
+# if Operator == "+":
+#     print (f"{Number_1}{Operator}{Number_2}={Number_1 + Number_2}")
+# elif Operator == "-":
+#     print (f"{Number_1}{Operator}{Number_2}={Number_1 - Number_2}")
+# elif Operator == "*":
+#     print (f"{Number_1}{Operator}{Number_2}={Number_1 * Number_2}")
+# elif Operator == "/":
+#     print (f"{Number_1}{Operator}{Number_2}={Number_1 / Number_2:.2f}")
 
-#Create a simple quiz progream with 3 questions. At the end of teh quiz, display score/
+# Exercise 2: Create a simple quiz progream with 3 questions. At the end of teh quiz, display score/
+
+score= 0
+
+q1 = input("What is the capital of Malaysia? : ")
+if q1 == "Kuala Lumpur" or q1 == "KL" or q1 == "kuala lumpur" or q1 == "kl":
+    score = score + 1
+else:
+    score = score + 0
+
+q2 = input("What language are we learning? : ")
+if q2 == "Python" or q2 == "python":
+    score = score + 1
+else:
+    score = score + 0
+
+q3 = int(input("6 + 7 : "))
+if q3 == 13:
+    score = score + 1
+else:
+    score = score + 0
+
+print(f"Your score: {score}/3")
 
