63104935bf4add0bf3255c2ab782c379688ec94d
defozecheng <defo.shin88@gmail.com>
2026-08-05 09:29:10 +0800

    your comment

---
+++ py/07_tuples.py
@@ -0,0 +1,13 @@
+# Tuples
+
+coordinates = (10,20)
+person = ("Alice", 25,"Engineer")
+single_item = (42,)                 # Note the comma for single item
+
+#Tuple operations
+# person.append("Georgetown")
+# single_item.insert(3,"Money")
+
+print(coordinates[0])               # Acessing elements
+print(len(person))                  # Length
+
